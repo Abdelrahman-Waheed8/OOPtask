b71aa4701e6d30704411fc71edeb0cc481cf2a4c
Abdelrahman-Waheed8 <abdothenoobest@gmail.com>
2026-09-25 15:18:20 +0300

    added t or f questions

--- Classes/TrueOrFalseQuestion.cs
+++ Classes/TrueOrFalseQuestion.cs
@@ -1,8 +1,19 @@
 public class TrueOrFalse : Question
 {
-    public TrueOrFalse(string header, string body, int mark, bool rightanswer) : base(header, body, mark)
+    public TrueOrFalse(string header, string body, int mark, Answer[] answerlist, Answer rightanswer) : base(header, body, mark, answerlist, rightanswer)
     {
-        AddAnswer(new Answer(1, "True"), rightanswer);
-        AddAnswer(new Answer(2, "False"), !rightanswer);
+    }
+
+    public override void ShowQuestion()
+    {
+        Console.WriteLine("------------------------------------------");
+        Console.WriteLine($"         True or False         {Mark} mark(s)  ");
+        Console.WriteLine("------------------------------------------");
+        Console.WriteLine($"{Header}\n{Body}");
+
+        foreach(var ans in Answers)
+        {
+            Console.WriteLine($"{ans._AnswerId}. {ans._AnswerText}");
+        }
     }
 }
