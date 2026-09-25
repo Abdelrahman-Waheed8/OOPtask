c3aead1ee2ea2ce3f1c239b44827fc485526f658
Abdelrahman-Waheed8 <abdothenoobest@gmail.com>
2026-09-25 16:25:37 +0300

    Added basic imp in main

--- Program.cs
+++ Program.cs
@@ -3,8 +3,24 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("================== Examination System ==================");
-        Console.WriteLine("--------------------------------------------------------");
-        Console.WriteLine("                      Create Exam                       ");
-        Console.WriteLine("--------------------------------------------------------");
+        Console.WriteLine("Choose Exam Type: ");
+        Console.WriteLine("1. Final");
+        Console.WriteLine("2. Practical");
+        Console.Write("Enter your exam choice: ");
+        while(!int.TryParse(Console.ReadLine(), out int examChoice) || examChoice <= 0 || examChoice > 2)
+        {
+            Console.Write("Invalid choice Please choose from 1 and 2: ");
+        }
+        Console.WriteLine("========================================================");
+
+        Console.Write("Enter exam time in minutes: ");
+        int.TryParse(Console.ReadLine(), out int timeOfExam);
+        Console.Write("Enter number of questions of exam");
+        int.TryParse(Console.ReadLine(), out int numberofQuestions);
+
+        while(true)
+        {
+            
+        }
     }
 }
